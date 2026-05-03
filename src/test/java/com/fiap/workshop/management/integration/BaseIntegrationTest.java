e3650307b2160b877743526fa598aa3bbfbd998b
Adriana-Meyer <drika_meyer@hotmail.com>
2026-05-03 16:04:58 -0300

    fix: fixed integration tests to connect correctly to TestContainer

--- src/test/java/com/fiap/workshop/management/integration/BaseIntegrationTest.java
+++ src/test/java/com/fiap/workshop/management/integration/BaseIntegrationTest.java
@@ -2,32 +2,27 @@ package com.fiap.workshop.management.integration;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
+@TestPropertySource(properties = {
+        "spring.jpa.hibernate.ddl-auto=validate",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQLDialect",
+        "spring.flyway.enabled=true"
+})
 public abstract class BaseIntegrationTest {
 
-    @Container
+    @ServiceConnection
+    @SuppressWarnings("resource")
     static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
             .withDatabaseName("workshop_db")
             .withUsername("workshop")
             .withPassword("workshop123");
 
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url",
-                () -> mysql.getJdbcUrl() + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true");
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
-        registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.MySQLDialect");
-        registry.add("spring.flyway.enabled", () -> "true");
+    static {
+        mysql.start();
     }
 }
